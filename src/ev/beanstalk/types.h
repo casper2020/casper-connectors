6da1762072bddfbefbef3d4132ae7ade05bb8ac8
Américo Gomes <americo.gomes@cldware.com>
2019-03-13 17:43:31 +0000

    Fix  src/ev/beanstalk/types.h

--- src/ev/beanstalk/types.h
+++ src/ev/beanstalk/types.h
@@ -43,9 +43,10 @@ namespace ev
             
             inline void operator=(const _Config& a_config)
             {
-                host_    = a_config.host_;
-                port_    = a_config.port_;
-                timeout_ = a_config.timeout_;
+                host_          = a_config.host_;
+                port_          = a_config.port_;
+                timeout_       = a_config.timeout_;
+                abort_polling_ = a_config.abort_polling_;
                 tube_    = a_config.tube_;
                 for ( auto it : sessionless_tubes_ ) {
                     sessionless_tubes_.insert(it);
