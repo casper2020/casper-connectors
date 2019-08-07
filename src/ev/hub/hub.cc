1065c6b8018b9201e6eb2fe2ae7fdf6673e63eac
Américo Gomes <americo.gomes@cldware.com>
2019-08-07 17:46:20 +0100

    TMP: hub idle check decreased to 50ms

--- src/ev/hub/hub.cc
+++ src/ev/hub/hub.cc
@@ -457,9 +457,8 @@ void ev::hub::Hub::Loop ()
     }
     
     timeval tv;
-    tv.tv_sec  = 15;
-    tv.tv_usec = 0;
-
+    tv.tv_sec  = 0;
+    tv.tv_usec = 50*1000;
     
     if ( 0 != event_add(socket_event_, &tv) ) {
         fault_msg_ = "Unable to add datagram socket event!";
