6e84685358edc8cb9e229a7804b41bcee89b4461
Américo Gomes <americo.gomes@cldware.com>
2021-11-19 17:57:29 +0000

    Updated license text.

--- src/cc/easy/http.h
+++ src/cc/easy/http.h
@@ -3,20 +3,20 @@
  *
  * Copyright (c) 2011-2020 Cloudware S.A. All rights reserved.
  *
- * This file is part of casper-fs-assistant.
+ * This file is part of casper-connectors.
  *
- * casper-fs-assistant is free software: you can redistribute it and/or modify
+ * casper-connectors is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * casper-fs-assistant  is distributed in the hope that it will be useful,
+ * casper-connectors  is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with casper-fs-assistant.  If not, see <http://www.gnu.org/licenses/>.
+ * along with casper-connectors. If not, see <http://www.gnu.org/licenses/>.
  */
 #pragma once
 #ifndef NRS_CC_EASY_HTTP_H_
