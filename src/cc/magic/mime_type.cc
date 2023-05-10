a402986c9a77d27ca42a0e7971f93afd000b5fd0
AmÃ©rico Gomes <americo.gomes@cloudware.pt>
2023-05-10 12:38:19 +0100

    Adding MIME Type class.

---
+++ src/cc/magic/mime_type.cc
@@ -0,0 +1,143 @@
+/**
+ * @file mime_type.cc
+ *
+ * Copyright (c) 2017-2023 Cloudware S.A. All rights reserved.
+ *
+ * This file is part of casper-connectors.
+ *
+ * casper-connectors is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * casper-connectors is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with casper-connectors. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "cc/magic/mime_type.h"
+
+#include "cc/exception.h"
+#include "cc/fs/dir.h"
+#include "cc/fs/file.h"
+
+#include "cc/types.h"
+#include "cc/debug/types.h"
+
+const std::string cc::magic::MIMEType::sk_pdf_ = { 0x25, 0x50, 0x44, 0x46, 0x2d }; // %PDF-
+
+/**
+ * @brief Default constructor.
+ */
+cc::magic::MIMEType::MIMEType ()
+{
+    cookie_ = nullptr;
+    buffer_[0] = 0;
+}
+
+/**
+ * @brief Destructor
+ */
+cc::magic::MIMEType::~MIMEType ()
+{
+    if ( nullptr != cookie_ ) {
+        magic_close(cookie_);
+    }
+}
+
+/**
+ * @brief Initilizer magic library.
+ *
+ * @param a_shared_directory Shared directory URI.
+ * @param a_flags            Default MAGIC_MIME_TYPE.
+ */
+void cc::magic::MIMEType::Initialize (const std::string& a_shared_directory, int a_flags)
+{
+    if ( nullptr != cookie_ ) {
+        return;
+    }
+    
+    cookie_ = magic_open(a_flags);
+    if ( nullptr == cookie_ ) {
+        throw cc::Exception("Unable to initialize magic library!");
+    }
+        
+#ifdef __APPLE__
+    const std::string mgc_file_uri = ::cc::fs::Dir::Normalize(a_shared_directory) + CC_IF_DEBUG_ELSE("libmagic/debug/magic.mgc", "libmagic/magic.mgc");
+#else
+    const std::string mgc_file_uri = ::cc::fs::Dir::Normalize(a_shared_directory) + "libmagic/magic.mgc";
+#endif
+    if ( 0 == magic_load(cookie_, mgc_file_uri.c_str() ) ) {
+        return;
+    }
+    
+    const std::string error = magic_error(cookie_);
+    magic_close(cookie_);
+    cookie_ = nullptr;
+    throw cc::Exception("Cannot load magic database from: %s - %s!", mgc_file_uri.c_str(), error.c_str());
+}
+
+/**
+ * @brief Obtain a file's MIME Type.
+ *
+ * @param a_uri Local file URI.
+ *
+ * @return File's MIME Type value ( / encode if previously requested ), "" if not found.
+ */
+std::string cc::magic::MIMEType::MIMETypeOf (const std::string& a_uri) const
+{
+    if ( nullptr == cookie_ ) {
+        throw cc::Exception("Magic library is not initialized!");
+    }
+    
+    if ( false == ::cc::fs::File::Exists(a_uri) ) {
+        throw cc::Exception("File %s does not exist!", a_uri.c_str());
+    }
+    
+    const char* const mime = magic_file(cookie_, a_uri.c_str());
+    if ( nullptr != mime ) {
+        return mime;
+    } else {
+        return "";
+    }
+}
+
+/**
+ * @brief Obtain a file's MIME Type.
+ *
+ * @param a_uri Local file URI.
+ *
+ * @return File's MIME Type value ( / encode if previously requested ), "" if not found.
+ *
+ * @return True PDF data offset.
+ */
+std::string cc::magic::MIMEType::MIMETypeOf (const std::string& a_uri, std::size_t& o_offset)
+{
+    std::string mime     = MIMETypeOf(a_uri);
+                o_offset = 0;
+    if ( 0 == strcasecmp(mime.c_str(), "application/octet-stream") ) {
+        // ... 2nd attempt: f#c%edup PDF ...
+        ::cc::fs::File reader;
+        try {
+            size_t        read = 0;
+            bool          eof  = false;
+            reader.Open(a_uri, ::cc::fs::File::Mode::Read);
+            if ( reader.Size() >= sk_pdf_.size() && ( read = reader.Read(buffer_, sizeof(buffer_) / sizeof(buffer_[0]), eof) ) >= sk_pdf_.size() && false == eof ) {
+                std::string_view sv(reinterpret_cast<const char*>(buffer_), read);
+                if ( ( o_offset = sv.find(sk_pdf_) ) != sv.npos ) {
+                    mime = "application/pdf";
+                }
+            }
+            reader.Close();
+        } catch (...) {
+            // ... eat it ...
+            reader.Close();
+        }
+    }
+    // ... done ...
+    return mime;
+}
---
+++ src/cc/magic/mime_type.h
@@ -0,0 +1,75 @@
+/**
+ * @file mime_type.h
+ *
+ * Copyright (c) 2017-2023 Cloudware S.A. All rights reserved.
+ *
+ * This file is part of casper-connectors.
+ *
+ * casper-connectors is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * casper-connectors is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with casper-connectors. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#ifndef CC_MAGIC_MIME_TYPE_H_
+#define CC_MAGIC_MIME_TYPE_H_
+
+#include "cc/non-copyable.h"
+#include "cc/non-movable.h"
+
+#include <set>
+#include <map>
+#include <string>
+
+#include "cc/pragmas.h"
+
+CC_DIAGNOSTIC_PUSH()
+CC_DIAGNOSTIC_IGNORED("-Wc++98-compat-extra-semi")
+#include "magic.h"
+CC_DIAGNOSTIC_POP()
+
+namespace cc
+{
+
+    namespace magic
+    {
+     
+        class MIMEType final : public cc::NonCopyable, public cc::NonMovable
+        {
+            
+            private: // Const Data
+            
+                static const std::string sk_pdf_;
+            
+            private: // Data
+                
+                magic_t       cookie_;
+                unsigned char buffer_[1024];
+                
+            public: // Constructor(s) / Destructor
+                
+                MIMEType ();
+                virtual ~MIMEType();
+            
+            public: // API Method(s) / Function(s)
+
+                void        Initialize (const std::string& a_shared_directory, int a_flags = MAGIC_MIME_TYPE);
+                std::string MIMETypeOf (const std::string& a_uri) const;
+                std::string MIMETypeOf (const std::string& a_uri, std::size_t& o_offset);
+            
+        }; // end of class 'MIMEType'
+        
+    }
+
+} // end of namespace 'cloudware'
+
+#endif // CC_MAGIC_MIME_TYPE_H_
